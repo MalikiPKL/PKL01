ff0884686a2ed1f1ab107ded088f30ff4b57fb0e
MalikiPKL <90675926+MalikiPKL@users.noreply.github.com>
2021-09-15 14:08:49 +0700

    Update r_pekerjaan.sql
    rest 1

--- r_pekerjaan.sql
+++ r_pekerjaan.sql
@@ -65,6 +65,99 @@ CREATE TABLE `riwayat_pelatihan` (
   `create_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `update_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `portofoliio_alumni`
+--
+
+CREATE TABLE `portofoliio_alumni` (
+  `id_portofolio_alumni` varchar(10) NOT NULL,
+  `gambar` varchar(20) NOT NULL,
+  `client` varchar(20) NOT NULL,
+  `deskripsi_karya` varchar(100) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `update_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE `register` (
+  `nama` varchar(20) NOT NULL,
+  `nis` varchar(12) NOT NULL,
+  `alamat` varchar(200) NOT NULL,
+  `tempat_lahir` varchar(20) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` enum('pria','wanita') NOT NULL,
+  `nomor_hp` int(14) NOT NULL,
+  `pekerjaan` varchar(12) NOT NULL,
+  `id_nis` varchar(12) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `update_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `riwayat_kuliah`
+--
+
+CREATE TABLE `riwayat_kuliah` (
+  `id_riwayat_kuliah` varchar(10) NOT NULL,
+  `nis` varchar(12) NOT NULL,
+  `kampus` varchar(100) NOT NULL,
+  `tahun_masuk` date NOT NULL DEFAULT current_timestamp(),
+  `tahun_lulus` date NOT NULL DEFAULT current_timestamp(),
+  `jenjang` enum('Diploma','S1','S2','S3','Gelar Akademik Baru','Bachelor','Master','Doctor') NOT NULL,
+  `fakultas` varchar(100) NOT NULL,
+  `prodi` varchar(100) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `update_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `riwayat_wirausaha`
+--
+
+CREATE TABLE `riwayat_wirausaha` (
+  `id_riwayat_wirausaha` varchar(10) NOT NULL,
+  `nama_usaha` varchar(50) NOT NULL,
+  `tahun_didirikan` date NOT NULL DEFAULT current_timestamp(),
+  `deskripsi_usaha` varchar(100) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `update_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skill`
+--
+
+CREATE TABLE `skill` (
+  `skill` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `nis` varchar(12) NOT NULL,
+  `id_user` varchar(20) NOT NULL,
+  `pass` varchar(200) NOT NULL,
+  `recovery_pass` varchar(200) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `update_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
